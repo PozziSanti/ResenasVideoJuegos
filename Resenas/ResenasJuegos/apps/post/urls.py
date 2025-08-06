6d54d9486999f980d09c677caed260db40fdb69c
sofiprodanov <sofiabprodanov@gmail.com>
2025-08-06 11:36:55 -0300

    Se agregan rutas, funcionalidades y form para comentario, puntuacion y vista de comentarios(reseñas)

--- Resenas/ResenasJuegos/apps/post/urls.py
+++ Resenas/ResenasJuegos/apps/post/urls.py
@@ -3,5 +3,5 @@ from apps.post.views import IndexView, PostDetailView
 
 urlpatterns = [
     path('home/', IndexView.as_view(), name='home'),
-    path('post/<slug:slug>/', PostDetailView.as_view(), name='detalle_post'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
--- Resenas/ResenasJuegos/apps/post/views.py
+++ Resenas/ResenasJuegos/apps/post/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView, DetailView
+from django.shortcuts import redirect
 from apps.post.models import Category, Post
+from apps.comment.models import Comment
+from apps.comment.forms import CommentForm
 
 
 class IndexView(TemplateView):
@@ -27,9 +30,31 @@ class PostDetailView(DetailView):
     template_name = 'pages/post_detail.html'
     context_object_name = 'post'
     slug_field = 'slug'
+    slug_url_kwarg = 'slug'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['next_post'] = Post.objects.filter(id__gt=self.object.id).order_by('id').first()
-        context['prev_post'] = Post.objects.filter(id__lt=self.object.id).order_by('-id').first()
-        return context
+        post = self.get_object()
+
+        context['comments'] = post.comment.filter(approbed=True).order_by('-created_at') #carga comentario actual
+        context['next_post'] = Post.objects.filter(id__gt=post.id).order_by('id').first() #Agrega todos los comentarios del post actual, ordenados del más nuevo al más viejo
+        context['prev_post'] = Post.objects.filter(id__lt=post.id).order_by('-id').first()
+        
+        #Agrega los botones de navegación (post siguiente y anterior).
+        if self.request.user.is_authenticated:
+            context['form'] = CommentForm()
+        return context #Si el usuario está logueado, le pasa el formulario para comentar
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not request.user.is_authenticated:
+            return redirect('login')
+
+        #Si el usuario no está logueado, lo manda a iniciar sesión
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = self.object
+            comment.save()
+        return redirect('post_detail', slug=self.object.slug) #Redirecciona a la misma página para que el comentario se vea en pantalla
