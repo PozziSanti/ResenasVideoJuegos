cdbabf4a012458b1db822f53696cd3c46f525243
sofiprodanov <sofiabprodanov@gmail.com>
2025-08-08 02:37:21 -0300

    Se agrega boton de contactanos con mailto en el footer del sidebar

--- Resenas/ResenasJuegos/apps/post/urls.py
+++ Resenas/ResenasJuegos/apps/post/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('home/', IndexView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),  # Sobre nosotros 
     path('terms/', TermsView.as_view(), name='terms'),  # Terminos y condiciones 
-    path('privacidad/', PrivacyPolicyView.as_view(), name='privacy'),  # Politica de Privacidad
+    path('privacy/', PrivacyPolicyView.as_view(), name='privacy'),  # Politica de Privacidad
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
