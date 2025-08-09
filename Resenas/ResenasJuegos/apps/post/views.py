90ac478b07a506c7d480edf8178699fb02a4c702
Sofia Prodanov <sofiabprodanov@gmail.com>
2025-08-08 23:28:41 -0300

    Primeros templates (#14)
    * Se agrega scrollbar en el body
    
    * Se configuran funcionalidades de la flip card y la visualizacion de la misma en home
    
    * Se comienza bloque de codigo de pag principal con juegos categorizados
    
    * Se realizan migraciones de comment, favorite, post y user
    
    * Se comienzan a crear las categorias de los juegos y sus post
    
    * Se configuran archivos para visualizacion en el index de las categorias
    
    * Se realizan modificaciones para funcionamiento de flip card
    
    * Se agregan rutas, funcionalidades y form para comentario, puntuacion y vista de comentarios(reseñas)
    
    * Se modifica ruta a detalle de post
    
    * template de detalle de post

--- Resenas/ResenasJuegos/apps/post/views.py
+++ Resenas/ResenasJuegos/apps/post/views.py
@@ -24,7 +24,6 @@ class IndexView(TemplateView):
         context['posts_por_categoria'] = posts_por_categoria
         print("🟢 CONTEXTO FINAL:", context) #TODO:SACAR
         return context
-
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
 
