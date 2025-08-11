13eafd684c8659448efd23727c8d5d65918493d6
adriansuarez1999 <131199588+adriansuarez1999@users.noreply.github.com>
2025-08-11 11:59:18 -0300

    Post crud (#24)
    * Fix URL routing to include post app URLs
    
    * Se cambia las views de post de funciones a clases
    
    * Implement CRUD functionality for posts and update sidebar links to filter by category
    
    * Add forms and views for post creation with image upload functionality

--- Resenas/ResenasJuegos/apps/post/views.py
+++ Resenas/ResenasJuegos/apps/post/views.py
@@ -9,7 +9,6 @@ from django.urls import reverse_lazy
 from .forms import PostForm
 # from apps.comment.models import Comment
 
-
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
 
@@ -30,15 +29,19 @@ class IndexView(TemplateView):
         print("🟢 CONTEXTO FINAL:", context) #TODO:SACAR
         return context
 
+
 class AboutView(TemplateView):
     template_name = 'pages/about.html'
+
 
 class TermsView(TemplateView):
     template_name = "pages/terms.html"
 
+
 class PrivacyPolicyView(TemplateView):
     template_name = 'pages/privacy.html'
 
+
 # Filtros post por titulo
 class PostTitleFilter(ListView):
     model = Post
@@ -64,81 +67,9 @@ class PostCategoryFilter (ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         category = self.kwargs.get('category') # Obtiene la categoría de los parámetros de la URL
-
         if category:
-            queryset = queryset.filter(title__icontains=category) 
+            queryset = queryset.filter(category__title__iexact=category) 
         
-        return queryset
-
-# TODO: si se decide poner el filtro de la fecha en el buscador, se lo puede agregar a la vista PostTitleFilter
-# Filtros post por fecha de publicación
-class PostDateFilter(ListView):
-    model = Post
-    template_name = 'post/post_list.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        date = self.request.GET.get('date')
-
-        if date:
-            queryset = queryset.filter(created_at__date=date) # busca el post por fecha de publicación
-        
-        return queryset
-
-# TODO: se lo puede poner para filtrar por fecha de inicio y fin 
-# fecha_inicio = self.request.GET.get('desde')
-# fecha_fin = self.request.GET.get('hasta')
-
-# if fecha_inicio and fecha_fin:
-#     queryset = queryset.filter(created_at__date__range=[fecha_inicio, fecha_fin])
-
-# filtros post por estrellas
-class PostStarFilter(ListView):
-    model = Post
-    template_name = 'post/post_list.html' 
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        queryset = super().get_queryset().annotate(avg_score=Coalesce(Avg('comment__score'), Value(0)))  # Calcula el promedio de las calificaciones de los comentarios
-        
-        score = self.request.GET.get('score') # valor que viene del formulario de búsqueda
-
-        if score:
-            try:
-                score = float(score)
-                queryset = queryset.filter(avg_score__gte=score) # filtra comentarios que tengan un valor de estrellas mayor o igual al ingresado
-            except ValueError:
-                pass      # si el valor no es un número válido, no se aplica el filtro
-        return queryset
-
-
-# Filtros post por titulo
-class PostTitleFilter(ListView):
-    model = Post
-    template_name = 'post/post_list.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        q = self.request.GET.get('q')
-
-        if q:
-            queryset = queryset.filter(title__icontains=q) # busca en el título de los posts
-        
-        return queryset
-
-# Filtros post por categoría
-class PostCategoryFilter (ListView):
-    model = Post
-    template_name = 'post/post_list.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        category = self.kwargs.get('category') # Obtiene la categoría de los parámetros de la URL
-        if category:
-            queryset = queryset.filter(title__icontains=category) 
         return queryset
     
     # TODO: sacar, ya que es solo para probar el filtro de categoría
@@ -147,6 +78,7 @@ class PostCategoryFilter (ListView):
         context['selected_category'] = self.kwargs.get('category', '')
         return context
 
+
 # TODO: si se decide poner el filtro de la fecha en el buscador, se lo puede agregar a la vista PostTitleFilter
 # Filtros post por fecha de publicación
 class PostDateFilter(ListView):
@@ -170,7 +102,6 @@ class PostDateFilter(ListView):
 # if fecha_inicio and fecha_fin:
 #     queryset = queryset.filter(created_at__date__range=[fecha_inicio, fecha_fin])
 
-
 # filtros post por estrellas
 class PostStarFilter(ListView):
     model = Post
@@ -189,6 +120,7 @@ class PostStarFilter(ListView):
             except ValueError:
                 pass      # si el valor no es un número válido, no se aplica el filtro
         return queryset
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -224,6 +156,7 @@ class PostDetailView(DetailView):
             comment.save()
         return redirect('post_detail', slug=self.object.slug) #Redirecciona a la misma página para que el comentario se vea en pantalla
 
+
 # CRUD PARA LOS POSTS
 
 # Crear un nuevo post
@@ -231,12 +164,11 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     form_class = PostForm
     template_name = 'post/post_create.html'
     success_url = reverse_lazy('home')    # Redirige a la lista de posts después de crear uno
-
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['request'] = self.request
         return kwargs
-
+# TODO: agregar funcion 403 para que aparezca imagen del michi
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -245,24 +177,20 @@ class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         user = self.request.user
         return user.is_staff or user.is_superuser  # Solo permite acceso a usuarios administradores y superusuarios
 
+
 # Actualizar un post existente
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    form_class = PostForm
+    model = Post
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+    fields = ['titulo', 'contenido', 'categoria', 'imagen']
     template_name = 'post_update.html'
     success_url = reverse_lazy('post_list')    # Redirige a la lista de posts después de crear uno
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['request'] = self.request
-        return kwargs
-
     def form_valid(self, form):
-        post = form.save(commit=False)
-        form.instance.author = self.request.user      # TODO: si se quiere que el autor del post cambie al editar, agregar: post.autor = self.request.user
+        post = form.save(commit=False)      # TODO: si se quiere que el autor del post cambie al editar, agregar: post.autor = self.request.user
         post.save()
-        return redirect (self.success_url)
+        return super().form_valid(form)    
     
     def test_func(self):
         user = self.request.user
@@ -284,8 +212,7 @@ class PostListView(ListView):
                 )
             )
         .order_by('-created_at')
-        )  
-# get_queryset se usa para optimizar la consulta y traer las imágenes relacionadas de una sola vez, además de calcular el promedio de puntuaciones
+        )  # get_queryset se usa para optimizar la consulta y traer las imágenes relacionadas de una sola vez, además de calcular el promedio de puntuaciones
 
 
 # Eliminar un post existente
