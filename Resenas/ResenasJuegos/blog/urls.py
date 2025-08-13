e5781438924dcca5cb21d89ae49a3b381264fea3
PozziSanti <smpozzi06@gmail.com>
2025-08-13 08:58:32 -0300

    Fix: Add missing closing bracket in urlpatterns and ensure newline at end of file

--- Resenas/ResenasJuegos/blog/urls.py
+++ Resenas/ResenasJuegos/blog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='home'), # Página principal 
     path('', include('apps.user.urls')), # URLs de usuarios
     path('', include('apps.post.urls')), # Rutas de la app de post
+]
 
 # Sirve archivos estáticos y media en modo DEBUG
 if settings.DEBUG:
