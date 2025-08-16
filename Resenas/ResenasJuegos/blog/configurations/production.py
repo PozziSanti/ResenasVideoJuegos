d3d3bf6f2bea4077f4bc80167bb4a0dfccbbbcd8
adriansuarez1999 <feradrian1999@gmail.com>
2025-08-16 17:22:38 -0300

    se modifica el archivo .gitignore para incluir la carpeta media y se actualiza el archivo production.py para pasarlo a development

--- Resenas/ResenasJuegos/blog/configurations/production.py
+++ Resenas/ResenasJuegos/blog/configurations/production.py
@@ -8,20 +8,20 @@ ALLOWED_HOSTS=["127.0.0.1","midominio-production.com"]
 #TODO:CONFIGURAR DB PARA PRODUCTION
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
 
         #En caso de usar una postres utilizo:
         # "ENGINE": "django.db.backends.postgresql",
 
         #En caso de usar una postres utilizo:
-        "ENGINE": "django.db.backends.mysql",
+        # "ENGINE": "django.db.backends.mysql",
 
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
+        # "NAME": os.getenv('DB_NAME'),
+        # "USER": os.getenv('DB_USER'),
+        # "PASSWORD": os.getenv('DB_PASSWORD'),
+        # "HOST": os.getenv('DB_HOST'),
+        # "PORT": os.getenv('DB_PORT'),
     }
 }
 
