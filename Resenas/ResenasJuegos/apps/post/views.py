2007d57d0b113768308985c2629fd453b98f0ab1
adriansuarez1999 <feradrian1999@gmail.com>
2025-08-19 16:47:43 -0300

    se agregan urls y views para el filtro de ordenamiento de post por alfabetizacion

--- Resenas/ResenasJuegos/apps/post/views.py
+++ Resenas/ResenasJuegos/apps/post/views.py
@@ -137,7 +137,25 @@ class PostStarFilter(ListView):
                 pass
 
         return queryset
-    
+
+
+# Filtros post por letras del alfabeto
+class PostAlphabetFilter(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        order = self.request.GET.get("order", "asc")  # asc por defecto
+        if order == "desc":
+            return Post.objects.all().order_by("-title")
+        return Post.objects.all().order_by("title")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["order"] = self.request.GET.get("order", "asc")
+        return context
+
 
 # CRUD PARA LOS POSTS
 # Detalle de un post
